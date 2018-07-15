49d929740f142037c99a5bef3e8bc5f3e57c0f18
Quoc Tai <ltquoctaidn98@gmail.com>
2018-07-15 09:15:05 +0700

    apply pagination

--- infrastructure/repositories/baseRepository.js
+++ infrastructure/repositories/baseRepository.js
@@ -1,3 +1,5 @@
+const Constant = require('../constants/constants');
+
 class BaseRepository
 {
     constructor() {
@@ -8,26 +10,33 @@ class BaseRepository
     }
 
     /**
-     * Implementation required
+     * Return model to use other methods
+     ** Implementation required
      */
     model() {
         throw new Error('You have to implement the method doSomething!');
     }
 
-    get(conditions = {}, options = {}) {
-        const pagination = {
-            limit: options.limit || 15,
-            skip: ((options.page || 1) - 1) * limit
-        };
-
+    preGet(conditions, options) {
         conditions.deletedAt = null;
-        let query = this.model.find(conditions);
-        // Put more conditions here
-        query.sort({ createdAt: -1 });
-        query.skip(pagination.skip);
-        query.limit(pagination.limit);
+        return this.model
+            .paginate(conditions, { page: options.page, limit: Constant.limit })
+            .sort({ createdAt: -1 });
+    }
 
-        return query;
+    async get(conditions = {}, options = {}) {
+        const data = await this.preGet(conditions, options);
+
+        data.firstPageUrl = `${options.pageUrl}?page=1`;
+        data.lastPageUrl = `${options.pageUrl}?page=${data.pages}`;
+        if (!options.page || (options.page >= 1 && options.page < data.pages)) {
+            data.nextPageUrl = `${options.pageUrl}?page=${(options.page || 1) + 1}`;
+        }
+        if (options.page > 1 && options.page <= data.pages) {
+            data.previousPageUrl = `${options.pageUrl}?page=${(options.page || 1) - 1}`;
+        }
+
+        return data;
     }
 
     getDetail(conditions, options = {}) {
