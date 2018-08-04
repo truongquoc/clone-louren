4f9456a26fd22c801dee5827d28ad6fdafe54d7a
Tai.ltq <ltquoctaidn98@gmail.com>
2018-08-04 11:05:30 +0700

    NEST: clone property types

---
+++ modules/propertyTypes/middleware/propertyTypeAuthorize.js
@@ -0,0 +1,24 @@
+const responseHelper = require('../../../helpers/responseHelper');
+const PropertyTypeRepositoryClass = require('../repositories/PropertyTypeRepository');
+
+const PropertyTypeRepository = new PropertyTypeRepositoryClass();
+
+const indexAuthorize = (req, res, next) => {
+    next();
+};
+
+const editAuthorize = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const check = await PropertyTypeRepository.checkExist({ _id: id });
+        // Authorize user
+        if (!check) {
+            return res.json(responseHelper.notFound());
+        }
+        next();
+    } catch (e) {
+        return res.json(responseHelper.error(e.message));
+    }
+};
+
+module.exports = { indexAuthorize, editAuthorize };
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const adminBlogArticleRouter = require('../modules/blogArticles/routes-admin');
 const adminBlogCategoryRouter = require('../modules/blogCategories/routes-admin');
 const adminBlogTagRouter = require('../modules/blogTags/routes-admin');
 const adminPropertyCategoryRouter = require('../modules/propertyCategories/routes-admin');
+const adminPropertyTypeRouter = require('../modules/propertyTypes/routes-admin');
 const handleExceptionHelper = require('../helpers/handleExceptionHelper');
 
 router.use((req, res, next) => {
@@ -19,6 +20,7 @@ router.use('/admin/blog/categories', adminBlogCategoryRouter);
 router.use('/admin/blog/tags', adminBlogTagRouter);
 
 router.use('/admin/property/categories', adminPropertyCategoryRouter);
+router.use('/admin/property/types', adminPropertyTypeRouter);
 
 router.use(handleExceptionHelper.handleException);
 
