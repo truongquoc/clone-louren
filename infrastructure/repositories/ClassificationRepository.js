e38887dcd784798fcf03be7daf2070d398eefdcc
Tai.ltq <ltquoctaidn98@gmail.com>
2018-08-04 09:01:51 +0700

    NEST: refactor classification modules

---
+++ infrastructure/repositories/ClassificationRepository.js
@@ -0,0 +1,55 @@
+const getSlug = require('speakingurl');
+const BaseRepository = require('./BaseRepository');
+
+class ClassificationRepository extends BaseRepository {
+    constructor() {
+        super();
+        if (new.target === ClassificationRepository) {
+            throw new TypeError('Cannot construct Abstract instances directly');
+        }
+    }
+
+    async create(data) {
+        let classification = await this.getDetailOnlyTrashed({
+            $or: [{ name: data.name }, { slug: getSlug(data.slug) }],
+        });
+        if (classification) {
+            classification.name = data.name;
+            classification.slug = data.slug;
+            classification.createdAt = new Date();
+            classification.deletedAt = null;
+
+            return classification.save();
+        }
+        classification = {
+            name: data.name,
+            slug: data.slug || data.name,
+        };
+
+        return this.baseCreate(classification);
+    }
+
+    async update(data, id) {
+        let classification = await this.checkExistOnlyTrashed({
+            _id: { $ne: id },
+            $or: [{ name: data.name }, { slug: getSlug(data.slug) }],
+        });
+        if (classification) {
+            // Move deleted articles from this category to the category which will be updated.
+            // call 1 function to handle it
+            await classification.remove();
+        }
+        classification = {
+            name: data.name,
+            slug: data.slug || data.name,
+        };
+
+        return this.model.findOneAndUpdate(
+            { _id: id, deletedAt: null },
+            classification,
+            { new: true },
+        );
+    }
+}
+
+module.exports = ClassificationRepository;
--- modules/blogCategories/repositories/BlogCategoryRepository.js
+++ modules/blogCategories/repositories/BlogCategoryRepository.js
@@ -1,47 +1,9 @@
-const getSlug = require('speakingurl');
 const BlogCategory = require('../models/BlogCategory');
-const BaseRepository = require('../../../infrastructure/repositories/BaseRepository');
+const ClassificationRepository = require('../../../infrastructure/repositories/ClassificationRepository');
 
-class BlogCategoryRepository extends BaseRepository {
+class BlogCategoryRepository extends ClassificationRepository {
     model() {
         return BlogCategory;
-    }
-
-    async create(data) {
-        let category = await this.getDetailOnlyTrashed({
-            $or: [{ name: data.name }, { slug: getSlug(data.slug) }],
-        });
-        if (category) {
-            category.name = data.name;
-            category.slug = data.slug;
-            category.createdAt = new Date();
-            category.deletedAt = null;
-
-            return category.save();
-        }
-        category = {
-            name: data.name,
-            slug: data.slug || data.name,
-        };
-
-        return this.baseCreate(category);
-    }
-
-    async update(data, id) {
-        let category = await this.checkExistOnlyTrashed({
-            _id: { $ne: id },
-            $or: [{ name: data.name }, { slug: getSlug(data.slug) }],
-        });
-        if (category) {
-            // Move deleted articles from this category to the category which will be updated.
-            await category.remove();
-        }
-        category = {
-            name: data.name,
-            slug: data.slug || data.name,
-        };
-
-        return this.model.findOneAndUpdate({ _id: id, deletedAt: null }, category, { new: true });
     }
 }
 
