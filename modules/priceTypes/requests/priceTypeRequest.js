0fdf6cbe50c964167c75ed15cef865b7470bfe8e
Tai.ltq <ltquoctaidn98@gmail.com>
2018-08-13 22:35:38 +0700

    NEST: price types

---
+++ modules/priceTypes/requests/priceTypeRequest.js
@@ -0,0 +1,41 @@
+const { check } = require('express-validator/check');
+const PriceTypeRepositoryClass = require('../repositories/PriceTypeRepository');
+
+const PriceTypeRepository = new PriceTypeRepositoryClass();
+
+const createPriceTypeRequest = [
+    check('name')
+        .not().isEmpty().withMessage('Tên không được bỏ trống')
+        .custom(async (value) => {
+            try {
+                const validate = await PriceTypeRepository.checkExist({ name: value });
+                if (validate) {
+                    throw new Error('Tên đã được sử dụng');
+                }
+                return true;
+            } catch (e) {
+                return Promise.reject(e.message);
+            }
+        }),
+];
+
+const editPriceTypeRequest = [
+    check('name')
+        .not().isEmpty().withMessage('Tên không được bỏ trống')
+        .custom(async (value, { req }) => {
+            try {
+                const validate = await PriceTypeRepository.checkExist({
+                    _id: { $ne: req.params.id },
+                    name: value,
+                });
+                if (validate) {
+                    throw new Error('Tên đã được sử dụng');
+                }
+                return true;
+            } catch (e) {
+                return Promise.reject(e.message);
+            }
+        }),
+];
+
+module.exports = { createPriceTypeRequest, editPriceTypeRequest };
