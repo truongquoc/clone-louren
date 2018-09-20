d17659a8a4c32e24f2a493af443707ad59d769e8
duongvietminhtri <duongvietminhtri@gmail.com>
2018-09-20 12:16:15 +0700

    cart changed

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -40,7 +40,7 @@ module.exports = (res) => {
     res.locals.strLimit = (string, limit, end = '...') => `${string.substr(0, limit)}${end}`;
 
     res.locals.formatPrice = (number) => {
-        number = Number(number).toLocaleString().replace(/,/g, '.');
+        number = parseFloat(number).toLocaleString().replace(/,/g, '.');
         return number;
     };
 
